d6703453f4eef6d8a0046fdc0a744c08aad36a10
xiao.liu <liuxiaoabc@gmail.com>
2023-12-01 17:30:17 +0800

    feat: first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup() {
+    return this.authService.signup();
+  }
+  signin() {
+    return this.authService.signin();
+  }
+}
